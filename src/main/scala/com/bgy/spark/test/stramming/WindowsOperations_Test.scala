343151545b67819ca8ff032b5463c5c1daa20b38
lifu13 <93421357+lifu13@users.noreply.github.com>
2021-10-30 19:48:51 +0800

    Update WindowsOperations_Test.scala

--- src/main/scala/com/bgy/spark/test/stramming/WindowsOperations_Test.scala
+++ src/main/scala/com/bgy/spark/test/stramming/WindowsOperations_Test.scala
@@ -17,11 +17,8 @@ object WindowsOperations_Test {
     val value2: DStream[(String, Int)] = value.flatMap(x => x.split(" ")).map(x => (x, 1))
     //val value1: DStream[(String, Int)] = value2.window(Seconds(12), Seconds(6))
     //val value3: DStream[(String, Int)] = value1.reduceByKey(_ + _)
-//    val value1: DStream[(String, Int)] = value2.reduceByKeyAndWindow((x:Int, y:Int) => x + y
-//      ,(x:Int, y:Int) => x - y
-//      , Seconds(12), Seconds(4)
-//    ).updateStateByKey((seq:Seq[Int],op:Option[Int])=>
-//      Some(seq.toList.sum+op.getOrElse(0)))
+  //多写一行试试范德萨富士达
+    //这是在github上操作的
     val value1: DStream[(String, Int)] = value2.reduceByWindow((x, y) => (x._1 + y._1, x._2 + y._2), Seconds(12), Seconds(4))
     value1.print()
     context.start()
