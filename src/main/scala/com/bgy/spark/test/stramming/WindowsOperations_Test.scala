2cde4f4f0ca98cae3f03b46334b4103adfd2cd46
lifu13 <18664321994@163.com>
2021-10-30 20:50:52 +0800

    aaa

--- src/main/scala/com/bgy/spark/test/stramming/WindowsOperations_Test.scala
+++ src/main/scala/com/bgy/spark/test/stramming/WindowsOperations_Test.scala
@@ -9,17 +9,18 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
  * @author lifu13
  * @create 2021-10-29 19:30
  */
-object WindowsOperations_Test {
-  def main(args: Array[String]): Unit = {
+object WindowsOperations_Test {  def main(args: Array[String]): Unit = {
     val context: StreamingContext = new StreamingContext(new SparkConf().setMaster("local[*]").setAppName("StramingContext_test"),Seconds(2))
     context.checkpoint("check")
     val value: ReceiverInputDStream[String] = context.socketTextStream("hadoop102", 9998)
     val value2: DStream[(String, Int)] = value.flatMap(x => x.split(" ")).map(x => (x, 1))
     //val value1: DStream[(String, Int)] = value2.window(Seconds(12), Seconds(6))
     //val value3: DStream[(String, Int)] = value1.reduceByKey(_ + _)
-  //多写一行试试范德萨富士达
-    //这是在github上操作的范德萨 发射点士大夫的撒法撒旦的撒发射点
+  //多写一行试试范德萨富士达fasd
+    //这是在github上操作的范德萨 发射点士大夫的撒法撒旦的发射点vdsfsfsdafsadfasdf
+    //fdsafsadfsadfasdfasdjlfsajldf
     val value1: DStream[(String, Int)] = value2.reduceByWindow((x, y) => (x._1 + y._1, x._2 + y._2), Seconds(12), Seconds(4))
+    value1.print()
     value1.print()
     context.start()
 
